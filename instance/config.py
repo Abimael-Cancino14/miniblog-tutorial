60b0eccba39ffe8b3ade19e64a21d99185969759
Abimael-Cancino14 <145639395+Abimael-Cancino14@users.noreply.github.com>
2023-09-22 13:37:36 -0600

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
 # instance/config.py
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:AbimaelBase140205@localhost:5432/miniblog'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres14:n9fcYI3IZw4VNQPpb5YxZxmAD9pY9WKS@dpg-ck6tvvo8elhc73e6i0jg-a/renderminiblog'
